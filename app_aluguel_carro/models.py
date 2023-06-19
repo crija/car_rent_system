21d42193062e651d35cd29c182310b79be172918
crija <crijacarla@gmail.com>
2023-06-19 16:14:31 -0300

    Add new views to render customer data

--- app_aluguel_carro/models.py
+++ app_aluguel_carro/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-class cadastro
+class Cadastro(models.Model):
+    id_cadastro = models.AutoField(primary_key=True)
+    nome = models.TextField(max_length=100)
+    idade = models.IntegerField()
+    gmail = models.TextField(max_length=200)
+    cpf = models.IntegerField()
+    cnh = models.IntegerField()
--- app_aluguel_carro/views.py
+++ app_aluguel_carro/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
+from .models import Cadastro
+
 
 def necessidades(request):
     return render(request, 'cadastro/necessidades.html')
 
 def cadastro(request):
-    pass
+    #Salvar os dados da tela para o banco de dados.
+    novo_cadastro = Cadastro()
+    novo_cadastro.nome = request.POST.get('nome')
+    novo_cadastro.idade = request.POST.get('idade')
+    novo_cadastro.cpf = request.POST.get('cpf')
+    novo_cadastro.cnh = request.POST.get('cnh')
+    novo_cadastro.gmail = request.POST.get('gmail')
+    novo_cadastro.save()
+
+    #Exibir todos os usuários já cadastrados em uma nova página.
+    cadastro = {
+        'cadastro':Cadastro.objects.all()
+    }
+
+    #Retornar os dados para a página de listagem de usuários
+    return render(request, 'cadastro/cadastro.html', cadastro)
