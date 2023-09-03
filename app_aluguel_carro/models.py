a0637784c921ed8ccfa88f3e76c91e3484bccc7f
crija <crijacarla@gmail.com>
2023-09-03 16:03:32 -0300

    Add restarting project

--- app_aluguel_carro/models.py
+++ app_aluguel_carro/models.py
@@ -1,17 +1,2 @@
 from django.db import models
 
-class Cadastros(models.Model):
-    id_cadastro = models.AutoField(primary_key=True)
-    nome = models.TextField(max_length=100)
-    idade = models.IntegerField()
-    gmail = models.TextField(max_length=200)
-    cpf = models.IntegerField()
-    cnh = models.IntegerField()
-
-class Preferencias(models.Model):
-    id_preferencias = models.AutoField(primary_key=True)
-    cidade_retirada = models.TextField(max_length=100)
-    dia_retirada = models.IntegerField()
-    dia_devolução = models.IntegerField()
-    tamanho = models.TextField(max_length=1)
-    cor = models.TextField(max_length=50)
--- projeto_aluguel_carro/urls.py
+++ projeto_aluguel_carro/urls.py
@@ -4,11 +4,5 @@ from django.urls import path
 from app_aluguel_carro import views
 
 urlpatterns = [
-    path('dados', views.dados, name = 'dados'),
-
-    path('cadastros/', views.cadastros, name = 'informações_cadastradas'),
-
-    path('preferencias/', views.preferencias, name ='informações_preferenciais'),
-
    
 ]
