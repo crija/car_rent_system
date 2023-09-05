ea7007cd537a83897dc3023ba81397cd2770a48e
crija <crijacarla@gmail.com>
2023-09-05 18:36:51 -0300

    Add create car endpoint

--- app_aluguel_carro/views.py
+++ app_aluguel_carro/views.py
@@ -2,6 +2,15 @@ from django.shortcuts import render
 from .models import Car
 
 def cars(request):
+    if request.method == "POST":
+        new_car = Car()
+        new_car.color = request.POST.get('color')
+        new_car.model = request.POST.get('model')
+        new_car.brand = request.POST.get('brand')
+        new_car.year = request.POST.get('year')
+        new_car.picture = request.POST.get('picture')
+        new_car.save()
+
     cars = Car.objects.all()
     context = {'cars': cars}
     return render(request, 'cars/index.html', context)
